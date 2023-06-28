55dffebdaff4b7370aa431f48a038f057fb5c38a
Bibek Neupane <bibek.neupane@amniltech.com>
2023-06-28 21:55:39 +0545

    Add basic component of the tic-tac-toe game

---
+++ js/Day27/tic-tac-toe/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Square(){
+  const[value,setValue] = useState(null);
+  function handleClick(){
+    //console.log('clicked!');
+    setValue('X');
+  }
+  return <button className="square" onClick={handleClick}>{value}</button>
+}
+export default function Board(){
+  return (
+    <>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
+    
+    </>
+    
+  );
+}
