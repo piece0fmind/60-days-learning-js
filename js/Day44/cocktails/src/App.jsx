565b6fc965effebb1a6e1943475fd3ab70e84a8e
Bibek Neupane <bibek.neupane@amniltech.com>
2023-07-17 21:34:48 +0545

    feat: Finish the cocktails app

--- js/Day44/cocktails/src/App.jsx
+++ js/Day44/cocktails/src/App.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './Components/NavBar';
+const Home = lazy(() => import('./Pages/Home'));
+const About = lazy(() => import('./Pages/About'));
+const Products = lazy(() => import('./Pages/Products'));
+const ProductDetails = lazy(() => import('./Pages/ProductDetails'));
+const NoMatch = lazy(() => import('./Components/NoMatch'));
 
-import App from './App';
-
-
-
-const App = () =>{
-  return(
-    <>
-    <Routes>
-      <Route path = "/" element = {<Home/>}/>
-      <Route path = "/products" element = {<Products/>}/>
-      <Route path = "/about" element = {<About/>}/>
-
-    </Routes>
-    </>
-  );
+const App = () => {
+	return (
+		<>
+			<NavBar />
+			<Suspense fallback={<div className="container">Loading...</div>}>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/products" element={<Products />} />
+					<Route path="/products/:slug" element={<ProductDetails />} />
+					<Route path="*" element={<NoMatch />} />
+				</Routes>
+			</Suspense>
+		</>
+	);
 };
 
 export default App;
